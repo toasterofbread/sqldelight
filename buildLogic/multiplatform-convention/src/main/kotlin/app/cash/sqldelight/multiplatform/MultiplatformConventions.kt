7e91ea4556b0c6922ac39c1cde7921a55df54cd4
Ilya Gulya <ilyagulya@gmail.com>
2024-03-25 18:23:39 +0500

    Try to use Kotlin 1.9.23 instead of 2.0.0-Beta5

--- buildLogic/multiplatform-convention/src/main/kotlin/app/cash/sqldelight/multiplatform/MultiplatformConventions.kt
+++ buildLogic/multiplatform-convention/src/main/kotlin/app/cash/sqldelight/multiplatform/MultiplatformConventions.kt
@@ -45,7 +45,7 @@ class MultiplatformConventions : Plugin<Project> {
         common {
           group("jsCommon") {
             withJs()
-            withWasmJs()
+            withWasm()
           }
         }
       }
